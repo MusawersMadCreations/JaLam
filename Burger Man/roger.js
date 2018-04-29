50ffd387d0cb7492f76e7301627633914c302ec6
Roge-L <36053941+Roge-L@users.noreply.github.com>
2018-04-29 17:26:27 -0600

    crap

--- Burger Man/roger.js
+++ Burger Man/roger.js
@@ -1,10 +1,17 @@
 let burger;
+let upTimer, downTimer, leftTimer, rightTimer;
 let person;
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
 
-  burger = new Burger(width/2, 100, 100, 100, 3);
+  burger = new Burger(width/2, 100, 100, 100, 0, 3);
+
+  upTimer = new Timer(1500);
+  downTimer = new Timer(1500);
+  leftTimer = new Timer(1500);
+  rightTimer = new Timer(1500);
+
   person = new Person();
 }
 
@@ -14,12 +21,12 @@ function draw() {
 }
 
 class Burger {
-  constructor(x, y, width, height, acceleration) {
+  constructor(x, y, width, height, velocity) {
     this.x = x;
     this.y = y;
     this.w = width;
     this.h = height;
-    this.a = acceleration;
+    this.v = velocity;
   }
 
   display() {
@@ -28,17 +35,19 @@ class Burger {
   }
 
   movement() {
+    // if one of the movement keys are pressed for at least one second, bump up acceleration
+    // timer for each arrow,
     if (keyIsDown(UP_ARROW)) {
-      this.y -= this.a;
+      this.y -= this.v;
     }
     if (keyIsDown(DOWN_ARROW)) {
-      this.y += this.a;
+      this.y += this.v;
     }
     if (keyIsDown(LEFT_ARROW)) {
-      this.x -= this.a;
+      this.x -= this.v;
     }
     if (keyIsDown(RIGHT_ARROW)) {
-      this.x += this.a;
+      this.x += this.v;
     }
   }
 }
@@ -97,6 +106,30 @@ class Onions {
   }
 }
 
+class Timer {
+  constructor(waitTime) {
+    this.waitTime = waitTime;
+    this.startTime = millis();
+    this.finishTime = this.startTime + this.waitTime;
+    this.timerIsDone = false;
+  }
+
+  reset(newWaitTime) {
+    this.waitTime = newWaitTime;
+    this.startTime = millis();
+    this.finishTime = this.startTime + this.waitTime;
+    this.timerIsDone = false;
+  }
+
+  isDone() {
+    if (millis() >= this.finishTime) {
+      return true;
+    }
+    else {
+      return false;
+    }
+  }
+}
 
 // needs
 // burger movement
